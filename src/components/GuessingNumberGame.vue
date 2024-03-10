6a86f1c5d46562a813360b4974ec0b525a127ea1
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-03-11 01:33:48 +0500

    The Login is done. now, I will improve user experience

--- src/components/GuessingNumberGame.vue
+++ src/components/GuessingNumberGame.vue
@@ -4,7 +4,12 @@
     <button @click="clickMethod" :disabled="click" :hidden="hidden">Click If You Want To Play The Game</button>
   </div>
 
-   <div class="mybox" v-show="click">
+    <div class="mybox" v-show="click">
+        <h2 class="header">Guess The Random Number</h2>
+        <input placeholder="Enter Your Guess!" type="number" class="inputField" v-model="mynumber">
+        <h1 v-if="mynumber === randomNumber">Congrats! You Guessed Correctly.</h1>
+        <h1 v-if="mynumber < randomNumber">Go Higher!</h1>
+        <h1 v-if="mynumber > randomNumber">Go Lower!</h1>
     </div>
 </template>
 
@@ -13,7 +18,9 @@ export default {
     data(){
         return{
             click: false,
-            hidden: false
+            hidden: false,
+            mynumber: '',
+            randomNumber: null
         }
     },
 
@@ -21,7 +28,9 @@ export default {
         clickMethod(){
             this.click = true
             this.hidden = true
-            
+            // it will generate a random number between 1 and 100
+            this.randomNumber = Math.floor(Math.random() * 100) + 1
+            console.log(this.randomNumber)
         }
     }
 }
@@ -63,11 +72,10 @@ button{
 button:hover{
     color: green;
     background: rgb(182, 181, 181);
+    cursor: pointer;
 }
 
 .mybox{
-    /* width: 600px; */
-    /* height: 400px; */
     width: 90vh;
     height: 60vh;
     background: white;
@@ -76,5 +84,55 @@ button:hover{
     left: 28%;
     border-radius: 20px;
 }
+
+.inputField{
+    width: 60vh;
+    height: 6vh;
+    position: absolute;
+    top: 25%;
+    left: 16%;
+    font-size: 19px;
+    font-weight: 300;
+    border: none;
+    background: rgb(221, 217, 217);
+    font-family: "Madimi One", sans-serif;
+    border-radius: 3px;
+    padding: 0px 15px;
+    color: rgb(83, 83, 83);
+}
+.inputField:focus{
+    background: white;
+    font-weight: 500px;
+    font-family: "Madimi One", sans-serif;
+    /* it will remove the border outline */
+    outline: none;
+}
+
+h1{
+    width: 100%;
+    height: 10%;
+    position: absolute;
+    top: 60%;
+    left: 0%;
+    font-weight: 500;
+    background: grey;
+    color: white;
+    text-align: center;
+}
+
+.header{
+    margin-top: 3%;
+    width: 100%;
+    height: 14%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: rgb(61, 60, 60);
+    color: white;
+    font-weight: 500;
+    text-align: center;
+}
+
+
 
 </style>
